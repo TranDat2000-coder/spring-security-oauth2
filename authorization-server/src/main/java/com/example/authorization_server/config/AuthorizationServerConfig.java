da68cc63784b1514f9432cc9e77e2c36985e8ef1
TranDat2000-coder <trandat.00712@gmail.com>
2024-07-11 20:51:20 +0700

    Update authorization

--- authorization-server/src/main/java/com/example/authorization_server/config/AuthorizationServerConfig.java
+++ authorization-server/src/main/java/com/example/authorization_server/config/AuthorizationServerConfig.java
@@ -1,44 +1,17 @@
 package com.example.authorization_server.config;
 
-import com.example.authorization_server.repository.ClientRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
-
-import java.util.stream.Collectors;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 
 @Configuration
 public class AuthorizationServerConfig {
-    
-    @Autowired
-    private ClientRepository clientRepository;
 
     @Bean
-    public RegisteredClientRepository registeredClientRepository() {
-        // Logic to load clients from the database
-        return new InMemoryRegisteredClientRepository(
-                clientRepository.findAll().stream().map(client -> RegisteredClient
-                        .withId(client.getId().toString())
-                        .clientId(client.getClientId())
-                        .clientSecret(new BCryptPasswordEncoder().encode(client.getClientSecret()))
-                        .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                        .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-                        .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-                        .redirectUri(client.getRedirectUri())
-                        .scope(client.getScopes())
-                        .build()
-                ).collect(Collectors.toList())
-        );
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
-    @Bean
-    public AuthorizationServerSettings authorizationServerSettings() {
-        return AuthorizationServerSettings.builder().build();
-    }
+
 }
