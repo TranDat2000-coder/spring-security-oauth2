5d7123d1f6c5d4472f0d6357a325a75fd517f7be
TranDat2000-coder <trandat.00712@gmail.com>
2024-07-13 09:55:12 +0700

    Update resource server

--- resource-server/src/main/java/com/home/resource_server/ResourceController.java
+++ resource-server/src/main/java/com/home/resource_server/ResourceController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ResourceController {
+
+    @GetMapping("/public")
+    public String homePage() {
+        return "Hello from Spring boot app";
+    }
     @GetMapping("/")
     String message() {
         return "Hello!";
